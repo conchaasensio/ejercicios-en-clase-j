b76dc2cd3176268f46780d71186a87d9b1dbf50d
Concha Asensio <conchaasensiomr@gmail.com>
2020-06-25 19:38:46 +0200

    Add exercise 1 lesson 2.6

--- modulo-2/leccion-2.6/ejercicio-1/main.js
+++ modulo-2/leccion-2.6/ejercicio-1/main.js
@@ -6,7 +6,13 @@ const adalaber1 = {
   profession: "periodista",
 };
 
-const message = `Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} y soy ${adalaber1.profession}`;
+const adalaber2 = {
+  name: "Rocío",
+  age: "25 años",
+  profession: "actriz",
+};
+
+const message = `Mi nombre es ${adalaber2.name}, tengo ${adalaber2.age} y soy ${adalaber2.profession}`;
 
 let body = document.querySelector("body");
 
