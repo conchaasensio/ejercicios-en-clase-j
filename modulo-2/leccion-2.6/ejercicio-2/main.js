814d910c64d929e3c1c82785121a609d260a9025
Concha Asensio <conchaasensiomr@gmail.com>
2020-06-25 19:50:01 +0200

    Add exercise 2 lesson 2.6

---
+++ modulo-2/leccion-2.6/ejercicio-2/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const adalaber1 = {};
+adalaber1.name = "Susana";
+adalaber1.age = "34 años";
+adalaber1.profession = "periodista";
+adalaber1.run = (phrase) => `Estoy corriendo.`;
+
+console.log(adalaber1.run());
