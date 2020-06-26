3c98aa6b24aaac8c4b8b9b3bb7d86159507912c2
Concha Asensio <conchaasensiomr@gmail.com>
2020-06-26 18:50:09 +0200

    Add exercise 7 lesson 2.6

---
+++ modulo-2/leccion-2.6/ejercicio-7/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const job = "developer";
+
+const user = {};
+user.firstName = "Concha";
+user.lastName = "Asensio";
+user.age = 33;
+user.job = job;
+
+user.firstName = "Laura";
+user.age++;
+
+console.log(user);
