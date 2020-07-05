e5c67a51db85428c5259bc9091c4514cb4ec390e
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-05 17:18:57 +0200

    Add lis, images and comments for each princess

--- modulo-2/modulo-2-practica-fin-de-semana-02-fetch/app/js/main.js
+++ modulo-2/modulo-2-practica-fin-de-semana-02-fetch/app/js/main.js
@@ -1,9 +1,31 @@
 'use strict';
 
-const ENDPOINT = 'https://beta.adalab.es/ejercicios-extra/js-fetch-arrays-princesas-disney/data/users.json'
+const ENDPOINT =
+  'https://raw.githubusercontent.com/conchaasensiomr/ejercicios-en-clase-j/master/modulo-2/modulo-2-practica-fin-de-semana-02-fetch/data/users.json';
+
+fetch(ENDPOINT)
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      const princess = data[i];
+
+      let ul = document.querySelector('.js-user-list');
+      let li = document.createElement('li');
+      li.appendChild(document.createTextNode(princess.name));
+      ul.appendChild(li);
+
+      let princessPicture = document.createElement('img');
+      princessPicture.src = princess.picture;
+      princessPicture.alt = 'Una princesa';
+      li.appendChild(princessPicture);
+
+      let princessComment = document.createElement('p');
+      princessComment.appendChild(document.createTextNode(princess.comment));
+      li.appendChild(princessComment);
+    }
+  });
 
 let users = [];
 let favorites = [];
 
 /* Do your magic! 🦄🦄🦄*/
-
