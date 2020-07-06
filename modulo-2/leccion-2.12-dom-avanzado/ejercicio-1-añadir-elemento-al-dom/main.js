c30a681ba829c1c92cee114de66ad4ee305697df
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-06 19:04:24 +0200

    Add exercise 1 lesson 2.12

---
+++ modulo-2/leccion-2.12-dom-avanzado/ejercicio-1-añadir-elemento-al-dom/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const numbers = [1, 2, 3];
+
+//Utilizo un bucle para crear un <li> por cada uno de los elementos que componen el array.
+for (const number of numbers) {
+  //Creamos el elemento <li>
+  const newItem = document.createElement('li');
+  //Creo contenido indicando como tal el elemento del array, en este caso, los números.
+  const newContent = document.createTextNode(number);
+  //Le añado el contenido al <li> mediante el método appendChild.
+  newItem.appendChild(newContent);
+  //Añado los elementos al DOM, incluyendo los <li> dentro del <ul> que ya existía en mi HTML.
+  const items = document.querySelector('.js-items');
+  items.appendChild(newItem);
+}
