a1771f357b9980c462488e0f7ecf2628f6a8b9aa
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-20 17:12:50 +0200

    use first class component

---
+++ modulo-3-react/leccion-3.3-intro-a-react-ii/my-react-app/src/components/RandomCat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class RandomCat extends React.Component {
+  render() {
+    return (
+      <a href="http://lorempixel.com">
+        <img src="http://lorempixel.com/400/200/cats/" alt="Random cat" />
+      </a>
+    );
+  }
+}
+export default RandomCat;
