53c32bfacdd1a7bd210aa50537462b895d972fc9
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-24 18:01:03 +0200

    add exercise 2 lesson 3.1

--- modulo-3-react/leccion-3.1-clases-de-e6/ejercicio-2/main.js
+++ modulo-3-react/leccion-3.1-clases-de-e6/ejercicio-2/main.js
@@ -15,7 +15,15 @@ class Square {
 }
 
 const mySquare1 = new Square(1);
+const mySquare2 = new Square(2);
+const mySquare3 = new Square(3);
 
 console.log(
   `El cuadrado 1 tiene un perímetro de ${mySquare1.perimeter()}cm y un área de ${mySquare1.area()}cm`
+);
+console.log(
+  `El cuadrado 1 tiene un perímetro de ${mySquare2.perimeter()}cm y un área de ${mySquare2.area()}cm`
+);
+console.log(
+  `El cuadrado 1 tiene un perímetro de ${mySquare3.perimeter()}cm y un área de ${mySquare3.area()}cm`
 );
