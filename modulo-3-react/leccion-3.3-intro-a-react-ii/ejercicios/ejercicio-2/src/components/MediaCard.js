307d636c4398f43d86fb78a76acb047bf48b9c27
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-26 10:46:22 +0200

    add exercise 2 lesson 3.3

--- modulo-3-react/leccion-3.3-intro-a-react-ii/ejercicios/ejercicio-2/src/components/MediaCard.js
+++ modulo-3-react/leccion-3.3-intro-a-react-ii/ejercicios/ejercicio-2/src/components/MediaCard.js
@@ -6,28 +6,20 @@ class MediaCard extends React.Component {
       <div className="App">
         <div className="profile-container">
           <div className="img-profile">
-            <img className="profile" src="./IMG_0026.jpg" alt="img-profile" />
+            <img className="profile" src={this.props.img} alt="img-profile" />
           </div>
           <div className="container-name">
-            <h1 className="name">Bilbo Asensio</h1>
-            <p>Viernes 17 de Julio de 2020</p>
+            <h1 className="name">{this.props.name}</h1>
+            <p>{this.props.date}</p>
           </div>
         </div>
         <div className="info-container">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            mattis vehicula tortor, sollicitudin maximus libero ultrices sit
-            amet. Suspendisse vitae quam neque. Cras sed hendrerit neque. Fusce
-            augue arcu, tincidunt interdum auctor vitae, placerat sit amet
-            massa. Cras eget tellus nec est maximus porta. Vivamus non
-            ullamcorper massa. Integer pharetra convallis felis. Donec at justo
-            non nunc finibus lobortis ut eu dolor.
-          </p>
+          <p>{this.props.text}</p>
 
           <div className="more">
             <p>Leer más...</p>
             <p>
-              37 <i className="fas fa-heart"></i>
+              {this.props.likes} <i className="fas fa-heart"></i>
             </p>
           </div>
         </div>
