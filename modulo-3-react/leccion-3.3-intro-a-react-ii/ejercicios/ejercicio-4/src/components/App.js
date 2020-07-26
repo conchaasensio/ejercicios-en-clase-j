38d3aefdcf3e20aa39b4686367f62bf67b9eb178
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-26 11:10:51 +0200

    add exercise 4 lesson 3.3

---
+++ modulo-3-react/leccion-3.3-intro-a-react-ii/ejercicios/ejercicio-4/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CatList from './CatList';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <CatList />
+      </div>
+    );
+  }
+}
+
+export default App;
