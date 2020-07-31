2cd537293a3cddff5055ecc5a2ea6908bc6b6365
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-31 17:57:30 +0200

    add exercise 4 lesson 3.4

---
+++ modulo-3-react/leccion-3.4-componentes-react/ejercicio-4/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../stylesheet/App.css';
+import ItemList from './ItemList';
+
+function App() {
+  return (
+    <div className="App">
+      <ItemList />
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo-3-react/leccion-3.4-componentes-react/ejercicio-4/src/components/ItemList.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Item from './Item';
+
+const arrayOfItems = [
+  {
+    name: 'Cereales con chocolate',
+    quantity: 2,
+    category: 'Cereales',
+    price: 5,
+  },
+  {
+    name: 'Hamburguesa con queso',
+    description: 'Hamburguesa rica y saludable',
+    quantity: 1,
+    category: 'Fast-food',
+    price: 15,
+  },
+  {
+    name: 'Agua mineral',
+    description: 'Agua de un charco del Himalaya',
+    quantity: 2,
+    category: 'Bebida',
+    price: 5,
+  },
+  {
+    name: 'Tarta de queso',
+    description: 'Hamburguesa rica y saludable',
+    quantity: 1,
+    category: 'Postres',
+    price: 4,
+  },
+];
+
+class ItemList extends React.Component {
+  render() {
+    const itemsLowerThan10 = arrayOfItems.filter((item) => item.price < 10);
+    const renderJSX = itemsLowerThan10.map((item) => {
+      return (
+        <li key={item.name}>
+          <Item
+            name={item.name}
+            description={item.description}
+            quantity={item.quantity}
+            category={item.category}
+            price={item.price}
+          />
+        </li>
+      );
+    });
+    return (
+      <div className="item-list">
+        <ul className="item-list">{renderJSX}</ul>
+      </div>
+    );
+  }
+}
+
+export default ItemList;
