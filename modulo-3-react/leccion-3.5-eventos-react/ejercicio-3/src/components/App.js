502fc911e501ebb541a355e2ebbc629816a77e15
Concha Asensio <conchaasensiomr@gmail.com>
2020-08-01 11:05:36 +0200

    add exercise 3 lesson 3.5

---
+++ modulo-3-react/leccion-3.5-eventos-react/ejercicio-3/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import OnionHater from './OnionHater';
+
+function App() {
+  return (
+    <div className="App">
+      <OnionHater />
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo-3-react/leccion-3.5-eventos-react/ejercicio-3/src/components/OnionHater.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class OnionHater extends React.Component {
+  constructor(props) {
+    super(props);
+    this.isHating = false;
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (ev) => {
+    const value = ev.target.value.toLowerCase();
+    const element = document.querySelector('.App');
+    if (value.includes('cebolla')) {
+      this.isHating = true;
+      element.classList.add('background');
+    } else {
+      this.isHating = false;
+      element.classList.remove('background');
+    }
+  };
+
+  render() {
+    return (
+      <div className="text">
+        <textarea
+          name="message"
+          id="message"
+          cols="30"
+          rows="10"
+          onChange={this.handleChange}
+        ></textarea>
+      </div>
+    );
+  }
+}
+
+export default OnionHater;
