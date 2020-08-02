7d514487f5cf8bf4f9f10680874523b3a40b176f
Concha Asensio <conchaasensiomr@gmail.com>
2020-08-02 12:30:30 +0200

    add exercise 8 lesson 3.5

---
+++ modulo-3-react/leccion-3.5-eventos-react/ejercicio-8/src/components/InputText.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const InputText = (props) => {
+  const handleKeyUp = (ev) => {
+    const data = {
+      name: props.name,
+      value: ev.currentTarget.value,
+    };
+    props.handleInput(data);
+  };
+
+  return (
+    <div>
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        id={props.id}
+        type="text"
+        name={props.name}
+        onKeyUp={handleKeyUp}
+      />
+    </div>
+  );
+};
+
+export default InputText;
---
+++ modulo-3-react/leccion-3.5-eventos-react/ejercicio-8/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Form from './components/Form';
+
+ReactDOM.render(<Form />, document.getElementById('root'));
